73d23f5c54b3746de0984414331ad3bc893fbbee
Getoar Krasniqi <geti2013@gmail.com>
2025-04-27 20:55:48 +0200

    shtimi i bedRoom dhe demetrin

--- src/Game.hs
+++ src/Game.hs
@@ -58,53 +58,91 @@ chain = MkItem { getUse = Nothing
                , getDisplay = "a chain"
                }
 
+demetrinUse :: State -> (State, String)
+demetrinUse state =
+    let
+        score = getScore $ getPlayer state
+        player = getPlayer state
+        inventory = getInventory player
+        inventory' = [ i | i <- inventory, i /= demetrinName ]
+        player' = MkPlayer { getInventory = inventory'
+                           , getLocation = getLocation player
+                           , getHealth = (getHealth player) + 2
+                           , getScore = (getScore player) + 50
+                           }
+        state' = MkState { getPlayer = player'
+                         , getItemLocations = getItemLocations state
+                         }
+        mesg = "...Now I am ready to sleep...\n"
+            ++ "\n Good night!! zzzz"
+    in
+        (state', mesg)
+
+demetrinName = "demetrin"
+demetrin = MkItem { getUse = Just demetrinUse
+               , getPut = True
+               , getTake = True
+               , getDisplay = "a demetrin"
+               }
+
 items = [ (bucketName, bucket)
         , (whiskeyName, whiskey)
         , (frogName, frog)
         , (chainName, chain)
         , (emptyWhiskeyName, emptyWhiskey)
+        , (demetrinName, demetrin)
         ]
 
-den = RoomName "Living Room"
-attic = RoomName "Attic"
-garden = RoomName "Garden"
+denName = RoomName "Living Room"
+atticName = RoomName "Attic"
+gardenName = RoomName "Garden"
+bedRoomName = RoomName "Bed Room"
 
 livingRoom :: Room
 livingRoom = MkRoom { getDescription = (Description "You are in the living-room. A wizard is snoring loudly on the couch.")
-                    , getRoomName = den
-                    , getExits = [ (Up, attic)
-                                 , (West, garden)
+                    , getRoomName = denName
+                    , getExits = [ (Up, atticName)
+                                 , (West, gardenName)
+                                 , (East, bedRoomName)]
+                    }
+
+bedRoom :: Room
+bedRoom = MkRoom { getDescription = (Description "You are in the bed room. Take a demetrin and get ready to sleep! zZzZz")
+                    , getRoomName = bedRoomName
+                    , getExits = [ (West, denName)
                                  ]
                     }
 
 atticRoom :: Room
 atticRoom = MkRoom { getDescription = (Description "You are in the attic. There is a giant welding torch in the corner.")
-                     , getRoomName = attic
-                     , getExits = [ (Down, den)]
+                     , getRoomName = atticName
+                     , getExits = [ (Down, denName)]
                      }
 
 gardenRoom :: Room
 gardenRoom = MkRoom { getDescription = (Description "You are in a beautiful garden. There is a well in front of you.")
-                    , getRoomName = garden
-                    , getExits = [(East, den)]
+                    , getRoomName = gardenName
+                    , getExits = [(East, denName)]
                     }
 
-rooms = [ (garden, gardenRoom)
-        , (den, livingRoom)
-        , (attic, atticRoom)
+rooms = [ (gardenName, gardenRoom)
+        , (denName, livingRoom)
+        , (atticName, atticRoom)
+        , (bedRoomName, bedRoom)
         ]
 
 player :: Player
 player = MkPlayer { getInventory = []
-                  , getLocation = den
+                  , getLocation = bedRoomName
                   , getHealth = 100
                   , getScore = 0
                   }
 
 state :: State
-state = MkState { getItemLocations = [ (garden, [frogName, chainName])
-                                     , (den, [whiskeyName, bucketName])
-                                     , (attic, [])
+state = MkState { getItemLocations = [ (gardenName, [frogName, chainName])
+                                     , (denName, [whiskeyName, bucketName])
+                                     , (atticName, [])
+                                     , (bedRoomName, [demetrinName])
                                      ]
                 , getPlayer = player
                 }
